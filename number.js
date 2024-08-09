b965bb34343d1191f6a1c06df4a24c92b236036a
Naiem Hasan <naiemjoy1@gmail.com>
2024-08-09 16:09:29 +0600

    number

---
+++ number.js
@@ -0,0 +1,9 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var applePrice = parseInt("20");
+
+console.log(typeof chocolate);
+console.log(typeof applePrice);
+console.log(typeof applePrice);
+
+console.log(orangePrice + applePrice);
